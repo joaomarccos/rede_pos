74f6fb5233a7babd9f339d508b3efa2047ada129
João Marcos Ferreira <joaomarccos.ads@gmail.com>
2019-11-03 23:10:59 -0300

    preparing for activity results

--- android/src/main/java/io/zafeapps/rede_pos_pg/RedePosPgPlugin.java
+++ android/src/main/java/io/zafeapps/rede_pos_pg/RedePosPgPlugin.java
@@ -1,26 +1,54 @@
 package io.zafeapps.rede_pos_pg;
 
+import android.app.Activity;
+import android.content.Intent;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
-/** RedePosPgPlugin */
-public class RedePosPgPlugin implements MethodCallHandler {
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "rede_pos_pg");
-    channel.setMethodCallHandler(new RedePosPgPlugin());
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-
-    if (call.method.equals("getPlatformVersion")) {
-      result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
-      result.notImplemented();
+/**
+ * RedePosPgPlugin
+ */
+public class RedePosPgPlugin implements MethodCallHandler, PluginRegistry.ActivityResultListener {
+    /**
+     * Plugin registration.
+     */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "rede_pos_pg");
+        RedePosPgPlugin plugin = new RedePosPgPlugin(channel, registrar.activity());
+        registrar.addActivityResultListener(plugin);
+        channel.setMethodCallHandler(plugin);
     }
-  }
+
+    private MethodChannel methodChannel;
+    private Result result;
+    private Activity activity;
+    private int requestCode;
+
+    public RedePosPgPlugin(MethodChannel methodChannel, Activity activity) {
+        this.methodChannel = methodChannel;
+        this.activity = activity;
+    }
+
+    @Override
+    public void onMethodCall(MethodCall call, Result result) {
+
+        if (call.method.equals("getPlatformVersion")) {
+            result.success("Android " + android.os.Build.VERSION.RELEASE);
+        } else {
+            result.notImplemented();
+        }
+
+
+    }
+
+    @Override
+    public boolean onActivityResult(int requestCode, int resultCode, Intent intent) {
+//    when receive a result from other activity do:
+        return false;
+    }
 }
